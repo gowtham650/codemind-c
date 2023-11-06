a8f0f5656340d13723214ea54e4e9e5c0fcc003c
gowtham650 <147539857+gowtham650@users.noreply.github.com>
2023-11-06 15:33:15 +0530

    #include<stdio.h> int main() {     int n,years,weeks;     scanf("%d",&n);     years=n/365;     weeks=(n%365)/7;     // printf("%d  %d",years,weeks);     printf("%d ",years);     printf("%d",weeks);
    }

---
+++ Days_into_years,_weeks:.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,years,weeks;
+    scanf("%d",&n);
+    years=n/365;
+    weeks=(n%365)/7;
+    // printf("%d
+ %d",years,weeks);
+    printf("%d
+",years);
+    printf("%d",weeks);
+    
+}
